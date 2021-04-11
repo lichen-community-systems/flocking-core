f2e454978a8879cc6804a8c4509b9227dd4bdbb1
Colin Clark <colinbdclark@gmail.com>
2021-04-11 11:27:17 -0400

    Resolves gh-6: Restructures the repository into a workspace.

--- examples/print_audio_tree/main.rs
+++
@@ -1,5 +0,0 @@
-extern crate libflock_cpal;
-
-fn main() {
-    libflock_cpal::print_audio_system_tree();
-}
---
+++ flocking_cpal/examples/print_audio_tree/main.rs
@@ -0,0 +1,5 @@
+extern crate flocking_cpal;
+
+fn main() {
+    flocking_cpal::print_audio_system_tree();
+}
